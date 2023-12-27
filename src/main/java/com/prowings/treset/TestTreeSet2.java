cb52cf5bf0f7b556379301654c5e2451ed62c7a6
JD <jayprowings@gmail.com>
2023-12-27 10:01:28 +0530

    navigable's methods demo

--- src/main/java/com/prowings/treset/TestTreeSet2.java
+++ src/main/java/com/prowings/treset/TestTreeSet2.java
@@ -1,5 +1,7 @@
 package com.prowings.treset;
 
+import java.util.Iterator;
+import java.util.Set;
 import java.util.TreeSet;
 
 public class TestTreeSet2 {
@@ -24,6 +26,41 @@ public class TestTreeSet2 {
 		System.out.println(set.tailSet(70));
 		
 		System.out.println("NavigableSet's headset() :"+set.headSet(70, true));
+		
+		System.out.println("NavigableSet's heigher() :"+set.higher(45));
+		System.out.println("NavigableSet's lower() :"+set.lower(30));
+
+		System.out.println("NavigableSet's ceiling() :"+set.ceiling(30));
+		System.out.println("NavigableSet's floor() :"+set.floor(30));
+		
+		Iterator itr = set.iterator();
+		
+		while(itr.hasNext())
+		{
+			System.out.println(itr.next());
+		}
+
+		System.out.println(">>>>>>>>>>decending iterator<<<<<<<<<<");
+		Iterator itr1 = set.descendingIterator();
+		
+		while(itr1.hasNext())
+		{
+			System.out.println(itr1.next());
+		}
+		System.out.println(">>>>>>>>>>decending set<<<<<<<<<<");
+		
+		Set decendingSet = set.descendingSet();
+		
+		System.out.println(decendingSet);
+		
+		System.out.println(set.pollFirst());
+		
+		System.out.println("After pollFirst() : "+set );
+
+		System.out.println(set.pollLast());
+		
+		System.out.println("After pollLast() : "+set );
 	}
 
+	
 }
